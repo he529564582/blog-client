6922273a2444c4769cb08b897d15f0f99f7d997c
he529564582 <he529564582@163.com>
2019-06-16 22:38:47 +0800

    增加路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Index from '@/page/Index/template.vue'
+import Login from '@/page/Login/template.vue'
+import Detail from '@/page/Detail/template.vue'
+import Edit from '@/page/Edit/template.vue'
+import Create from '@/page/Create/template.vue'
+import User from '@/page/User/template.vue'
+import My from '@/page/My/template.vue'
+import Register from '@/page/Register/template.vue'
 
 Vue.use(Router)
 
@@ -8,8 +15,35 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      component: Index
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/detail',
+      component: Detail
+    },
+    {
+      path: '/edit',
+      component: Edit
+    },
+    {
+      path: '/create',
+      component: Create
+    },
+    {
+      path: '/my',
+      component: My
+    },
+    {
+      path: '/user',
+      component: User
+    },
+    {
+      path: '/register',
+      component: Register
+    },
   ]
 })
