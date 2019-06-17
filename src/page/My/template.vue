e485448b63523e49a91ee8f520d5358016dbe275
he529564582 <he529564582@163.com>
2019-06-17 23:18:22 +0800

    增加my页面模板和less

--- src/page/My/template.vue
+++ src/page/My/template.vue
@@ -1,6 +1,28 @@
 <template>
-  <div class="my">
-   我的页面
+  <div id="my">
+    <section class="user-info">
+      <img src="http://cn.gravatar.com/avatar/2?s=128&d=identicon" alt="" class="avatar">
+      <h3>何方</h3>
+    </section>
+    <section>
+      <div class="item">
+        <div class="date">
+          <span class="day">20</span>
+          <span class="mouth">5月</span>
+          <span class="year">2019</span>
+        </div>
+        <h3>前端异步大揭秘</h3>
+        <p>Promise 是异步编程的一种解决方案，比传统的解决方案——回调函数和事件——更合理和更强大。它由社区最早提出和实现，ES6 将其写进了语言标准，统一了用法，原生提供了Promise对象。
+
+  所谓Promise，简单说就是一个容器，里面保存着某个未来才会结束的事件（通常是一个异步操作）的结果。从语法上说，Promise 是一个对象，从它可以获取异步操作的消息。Promise 提供统一的 API，各种异步操作都可以用同样的方法进行处理。
+
+  Promise对象有以下两个特点。</p>
+        <div class="actions">
+          编辑
+          <a href="#">删除</a>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 <style lang="less" scoped src="./template.less"></style>
