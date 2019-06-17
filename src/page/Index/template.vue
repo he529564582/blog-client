89f8f5aa4ba7967a687e8a43d71750670934f26b
he529564582 <he529564582@163.com>
2019-06-17 23:19:14 +0800

    增加首页less和template

--- src/page/Index/template.vue
+++ src/page/Index/template.vue
@@ -1,6 +1,46 @@
 <template>
-  <div class="index">
-   首页
+  <div id="index">
+    <section class="blog-posts">
+      <div class="item">
+        <figure class="avatar">
+          <img src="http://cn.gravatar.com/avatar/2?s=128&d=identicon" alt="">
+          <figcaption>何方</figcaption>
+        </figure>
+        <h3>前端异步大揭秘</h3>
+        <p>参数	说明	类型	可选值	默认值
+        infinite-scroll-disabled	是否禁用	boolean	-	false
+        infinite-scroll-delay	节流时延，单位为ms	number	-	200
+        infinite-scroll-distance	触发加载的距离阈值，单位为px	number	-	0
+        infinite-scroll-immediate	是否立即执行加载方法，以防初始状态下内容无法撑满容器。	boolean	-	true</p>
+        </div>
+    </section>
+        <section class="blog-posts">
+      <div class="item">
+        <figure class="avatar">
+          <img src="http://cn.gravatar.com/avatar/2?s=128&d=identicon" alt="">
+          <figcaption>何方</figcaption>
+        </figure>
+        <h3>前端异步大揭秘</h3>
+        <p>参数	说明	类型	可选值	默认值
+        infinite-scroll-disabled	是否禁用	boolean	-	false
+        infinite-scroll-delay	节流时延，单位为ms	number	-	200
+        infinite-scroll-distance	触发加载的距离阈值，单位为px	number	-	0
+        infinite-scroll-immediate	是否立即执行加载方法，以防初始状态下内容无法撑满容器。	boolean	-	true</p>
+        </div>
+    </section>    <section class="blog-posts">
+      <div class="item">
+        <figure class="avatar">
+          <img src="http://cn.gravatar.com/avatar/2?s=128&d=identicon" alt="">
+          <figcaption>何方</figcaption>
+        </figure>
+        <h3>前端异步大揭秘</h3>
+        <p>参数	说明	类型	可选值	默认值
+        infinite-scroll-disabled	是否禁用	boolean	-	false
+        infinite-scroll-delay	节流时延，单位为ms	number	-	200
+        infinite-scroll-distance	触发加载的距离阈值，单位为px	number	-	0
+        infinite-scroll-immediate	是否立即执行加载方法，以防初始状态下内容无法撑满容器。	boolean	-	true</p>
+        </div>
+    </section>
   </div>
 </template>
 <style lang="less" src="./template.less"></style>
@@ -8,4 +48,3 @@
 
 
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
