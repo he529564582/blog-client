10b346e136ad63571fda110050c2ccba98ce6241
he529564582 <he529564582@163.com>
2019-06-18 22:23:29 +0800

    更新登录页面

--- src/page/Login/template.vue
+++ src/page/Login/template.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="login">
-   登录页面
+  <div id="login">
+    <h4>用户名</h4>
+    <input v-model="username" placeholder="用户名">
+    <h4>密码</h4>
+    <input v-model="password" type="password" placeholder="密码" @keyup.enter="onLogin" >
+    <el-button size="medium" @click="onLogin">立即登录</el-button>
+    <p class="notice">没有账号？<router-link to="/register">注册新用户</router-link></p>
   </div>
 </template>
-<style lang="less" scoped src="./template.less"></style>
+
 <script src="./template.js"></script>
 
-
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./template.less" lang="less"></style>
