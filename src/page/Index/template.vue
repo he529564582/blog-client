a114721ec39354dc452b5b667a6a92f338677b2d
he529564582 <he529564582@163.com>
2019-06-18 22:23:43 +0800

    更新首页

--- src/page/Index/template.vue
+++ src/page/Index/template.vue
@@ -1,50 +1,26 @@
 <template>
   <div id="index">
     <section class="blog-posts">
-      <div class="item">
+      <!-- <router-link class="item" v-for="blog in blogs" :to="`/detail/${blog.id}`"> -->
         <figure class="avatar">
-          <img src="http://cn.gravatar.com/avatar/2?s=128&d=identicon" alt="">
-          <figcaption>何方</figcaption>
+          <img :src="blog.user.avatar" :alt="blog.user.username">
+          <figcaption>{{blog.user.username}}</figcaption>
         </figure>
-        <h3>前端异步大揭秘</h3>
-        <p>参数	说明	类型	可选值	默认值
-        infinite-scroll-disabled	是否禁用	boolean	-	false
-        infinite-scroll-delay	节流时延，单位为ms	number	-	200
-        infinite-scroll-distance	触发加载的距离阈值，单位为px	number	-	0
-        infinite-scroll-immediate	是否立即执行加载方法，以防初始状态下内容无法撑满容器。	boolean	-	true</p>
-        </div>
+        <h3>{{blog.title}}<span> {{friendlyDate(blog.createdAt)}}</span></h3>
+        <p>{{blog.description}}</p>
+      </router-link>
     </section>
-        <section class="blog-posts">
-      <div class="item">
-        <figure class="avatar">
-          <img src="http://cn.gravatar.com/avatar/2?s=128&d=identicon" alt="">
-          <figcaption>何方</figcaption>
-        </figure>
-        <h3>前端异步大揭秘</h3>
-        <p>参数	说明	类型	可选值	默认值
-        infinite-scroll-disabled	是否禁用	boolean	-	false
-        infinite-scroll-delay	节流时延，单位为ms	number	-	200
-        infinite-scroll-distance	触发加载的距离阈值，单位为px	number	-	0
-        infinite-scroll-immediate	是否立即执行加载方法，以防初始状态下内容无法撑满容器。	boolean	-	true</p>
-        </div>
-    </section>    <section class="blog-posts">
-      <div class="item">
-        <figure class="avatar">
-          <img src="http://cn.gravatar.com/avatar/2?s=128&d=identicon" alt="">
-          <figcaption>何方</figcaption>
-        </figure>
-        <h3>前端异步大揭秘</h3>
-        <p>参数	说明	类型	可选值	默认值
-        infinite-scroll-disabled	是否禁用	boolean	-	false
-        infinite-scroll-delay	节流时延，单位为ms	number	-	200
-        infinite-scroll-distance	触发加载的距离阈值，单位为px	number	-	0
-        infinite-scroll-immediate	是否立即执行加载方法，以防初始状态下内容无法撑满容器。	boolean	-	true</p>
-        </div>
+    <section class="pagination">
+      <el-pagination
+        layout="prev, pager, next"
+        :total="total"
+        :current-page="page"
+        @current-change="onPageChange">
+      </el-pagination>
     </section>
   </div>
 </template>
-<style lang="less" src="./template.less"></style>
+
 <script src="./template.js"></script>
 
-
-
+<style scoped lang="less" src="./template.less"></style>
