c8e6a3cc4ea34ea00f7a59a52a76867e3a5e2fb9
he529564582 <he529564582@163.com>
2019-06-18 22:22:49 +0800

    更新我的页面

--- src/page/My/template.vue
+++ src/page/My/template.vue
@@ -1,33 +1,37 @@
 <template>
-  <div id="my">
+  <div id="user">
     <section class="user-info">
-      <img src="http://cn.gravatar.com/avatar/2?s=128&d=identicon" alt="" class="avatar">
-      <h3>何方</h3>
+      <img :src="user.avatar" :alt="user.username" class="avatar">
+      <h3>{{user.username}}</h3>
     </section>
     <section>
-      <div class="item">
+      <router-link class="item" v-for="blog in blogs" :key="blog.id" :to="`/detail/${blog.id}`">
         <div class="date">
-          <span class="day">20</span>
-          <span class="mouth">5月</span>
-          <span class="year">2019</span>
+          <span class="day">{{splitDate(blog.createdAt).date}}</span>
+          <span class="month">{{splitDate(blog.createdAt).month}}月</span>
+          <span class="year">{{splitDate(blog.createdAt).year}}</span>
         </div>
-        <h3>前端异步大揭秘</h3>
-        <p>Promise 是异步编程的一种解决方案，比传统的解决方案——回调函数和事件——更合理和更强大。它由社区最早提出和实现，ES6 将其写进了语言标准，统一了用法，原生提供了Promise对象。
-
-  所谓Promise，简单说就是一个容器，里面保存着某个未来才会结束的事件（通常是一个异步操作）的结果。从语法上说，Promise 是一个对象，从它可以获取异步操作的消息。Promise 提供统一的 API，各种异步操作都可以用同样的方法进行处理。
-
-  Promise对象有以下两个特点。</p>
+        <h3>{{blog.title}}</h3>
+        <p>{{blog.description}}</p>
         <div class="actions">
-          编辑
-          <a href="#">删除</a>
+          <router-link :to="`/edit/${blog.id}`">编辑</router-link>
+          <a href="#" @click.prevent="onDelete(blog.id)">删除</a>
         </div>
-      </div>
+      </router-link>
+    </section>
+    <section class="pagination">
+      <el-pagination
+        layout="prev, pager, next"
+        :total="total"
+        :current-page="page"
+        @current-change="onPageChange">
+      </el-pagination>
     </section>
   </div>
 </template>
-<style lang="less" scoped src="./template.less"></style>
+
+
+
 <script src="./template.js"></script>
 
-
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./template.less" lang="less"></style>
