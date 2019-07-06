619a21d33f720a08d2fc0995e1362984c6263d72
he529564582 <he529564582@163.com>
2019-07-06 21:47:10 +0800

    fix  favicon路径bug

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -63,7 +63,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: config.build.index,
       template: 'index.html',
-      favicon: 'src/assets/favicon.ico',
+      // favicon: 'src/assets/favicon.ico',
       inject: true,
       minify: {
         removeComments: true,
